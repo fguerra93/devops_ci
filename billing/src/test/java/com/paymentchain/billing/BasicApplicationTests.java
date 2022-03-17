7ed1d1866595747902871769ce889dcd482d6bbe
Admin <admin@admin.com>
2022-03-17 00:17:43 -0300

    add integrating test using mockito, junit spring framework test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -7,7 +7,7 @@ import org.postgresql.translation.messages_bg;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//ddddd
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
